5fe9f46e66a42428bee1e9cf10748292733f9d76
liuyulong <liuyulong@qq.com>
2021-05-19 17:09:33 +0800

    ==

--- src/main/java/p/net/socket/SocketDemo.java
+++ src/main/java/p/net/socket/SocketDemo.java
@@ -6,22 +6,22 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class SocketDemo {
 	public static void main(String[] args) {
 		Socket socket = new Socket();
 		try {
 			socket.connect(new InetSocketAddress("localhost", 8090));
+			socket.setKeepAlive(true);
 			System.out.println("connect to server successfully");
-			new Thread(new ReadRunnable(socket)).start();
-//			new Thread(new WriteRunnable(socket)).start();
+//			new Thread(new ReadRunnable(socket)).start();
+			new Thread(new WriteRunnable(socket)).start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static class ReadRunnable implements Runnable {
+	static class ReadRunnable implements Runnable {
 		private Socket socket;
 
 		public ReadRunnable(Socket socket) {
@@ -35,18 +35,20 @@ public class SocketDemo {
 				byte[] bys = new byte[1024];
 				int avi = is.available();
 				System.out.println(avi);
-				is.read(bys);
-				System.out.println(new String(bys));
+				if(avi > 0) {
+					is.read(bys);
+					System.out.println(new String(bys));
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
-				try {
-					if (socket != null) {
-						socket.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+//				try {
+//					if (socket != null) {
+//						socket.close();
+//					}
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
 			}
 
 		}
@@ -62,27 +64,19 @@ public class SocketDemo {
 
 		@Override
 		public void run() {
-			try (InputStream input = socket.getInputStream();
-					Scanner sc = new Scanner(System.in);
-					OutputStream output = socket.getOutputStream();
+			try (OutputStream output = socket.getOutputStream(); 
 					PrintWriter pw = new PrintWriter(output);) {
-				while (sc.hasNext()) {
-					String line = sc.nextLine();
-					System.out.println(line);
-					if (line.equals("end")) {
-						break;
-					}
-					pw.write(line + "\n");
-					pw.flush();
-				}
+				pw.write("hello\n");
+				pw.flush();
+				System.out.println("done");
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+//				try {
+//					socket.close();
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
 			}
 
 		}
--- src/main/java/p/net/socket/nio/ServerSocketChannelDemo.java
+++ src/main/java/p/net/socket/nio/ServerSocketChannelDemo.java
@@ -10,6 +10,8 @@ import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerSocketChannelDemo {
 	public static void main(String[] args) {
@@ -17,14 +19,14 @@ public class ServerSocketChannelDemo {
 			// 创建 ServerSocketChannel 通道，绑定监听端口为 8080
 			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 			serverSocketChannel.socket().bind(new InetSocketAddress(8090));
-			// 设置为非阻塞模式
-			serverSocketChannel.configureBlocking(false);
-			// 注册选择器 , 设置选择器选择的操作类型
+			System.out.println("ServerSocketChannel run");
 			Selector selector = Selector.open();
-			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+			new Thread(new Acceptor(serverSocketChannel, selector), "Acceptor").start();
+
+			ExecutorService pool = Executors.newFixedThreadPool(2);
 
 			while (true) {
-				// 等待请求，每次等待阻塞 3s ，超过时间则向下执行，若传入 0 或不传值，则在接收到请求前一直阻塞
 				if (selector.select(3000) == 0) {
 					System.out.println(" 等待请求超时 ......");
 					continue;
@@ -35,62 +37,75 @@ public class ServerSocketChannelDemo {
 				Iterator<SelectionKey> keyIterator = keys.iterator();
 				while (keyIterator.hasNext()) {
 					SelectionKey selectionKey = keyIterator.next();
-					try {
-						// 如果是连接请求，调用处理器的连接处理方法
-						if (selectionKey.isAcceptable()) {
-							handleAccept(selectionKey);
-						}
-						// 如果是读请求，调用对应的读方法
-						if (selectionKey.isReadable()) {
-							handleRead(selectionKey);
-						}
-					} catch (IOException e) {
+					// 如果是读请求，调用对应的读方法
+					if (selectionKey.isReadable()) {
 						keyIterator.remove();
-						continue;
+						pool.execute(new ReadTask(selectionKey));
 					}
 				}
-				// 处理完毕从待处理集合移除该选择键
-				keyIterator.remove();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	/*
-	 * 连接请求处理方法
-	 */
-	public static void handleAccept(SelectionKey selectionKey) throws IOException {
-		// 通过选择器键获取服务器套接字通道，通过 accept() 方法获取套接字通道连接
-		SocketChannel socketChannel = ((ServerSocketChannel) selectionKey.channel()).accept();
-		// 设置套接字通道为非阻塞模式
-		socketChannel.configureBlocking(false);
-		// 为套接字通道注册选择器，该选择器为服务器套接字通道的选择器，即选择到该 SocketChannel 的选择器
-		// 设置选择器关心请求为读操作，设置数据读取的缓冲器容量为处理器初始化时候的缓冲器容量
-		socketChannel.register(selectionKey.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(1024));
-	}
+	static class Acceptor implements Runnable {
+		ServerSocketChannel serverSocketChannel;
+		Selector selector;
 
-	public static void handleRead(SelectionKey selectionKey) throws IOException {
-		// 获取套接字通道
-		SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-		// 获取缓冲器并进行重置 ,selectionKey.attachment() 为获取选择器键的附加对象
-		ByteBuffer byteBuffer = (ByteBuffer) selectionKey.attachment();
-		byteBuffer.clear();
-		// 没有内容则关闭通道
-		if (socketChannel.read(byteBuffer) == -1) {
-			socketChannel.close();
-		} else {
-			// 将缓冲器转换为读状态
-			byteBuffer.flip();
-			// 将缓冲器中接收到的值按 localCharset 格式编码保存
-			String receivedRequestData = Charset.forName("UTF-8").newDecoder().decode(byteBuffer).toString();
-			System.out.println(" 接收到客户端的请求数据： " + receivedRequestData);
-			// 返回响应数据给客户端
-			String responseData = " 已接收到你的请求数据，响应数据为： ( 响应数据 )";
-			byteBuffer = ByteBuffer.wrap(responseData.getBytes("UTF-8"));
-			socketChannel.write(byteBuffer);
-			// 关闭通道
-			socketChannel.close();
+		Acceptor(ServerSocketChannel serverSocketChannel, Selector selector) {
+			this.serverSocketChannel = serverSocketChannel;
+			this.selector = selector;
+		}
+
+		@Override
+		public void run() {
+			System.out.println("Acceptor run");
+			while (true) {
+				try {
+					SocketChannel socketChannel = serverSocketChannel.accept();
+					System.out.println("accept socket" + socketChannel.getRemoteAddress());
+					socketChannel.configureBlocking(false);
+					socketChannel.register(selector, SelectionKey.OP_READ);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
+
+	static class ReadTask implements Runnable {
+		SelectionKey selectionKey;
+
+		ReadTask(SelectionKey selectionKey) {
+			this.selectionKey = selectionKey;
+		}
+
+		@Override
+		public void run() {
+			// 获取套接字通道
+			SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+			// 获取缓冲器并进行重置 ,selectionKey.attachment() 为获取选择器键的附加对象
+			ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+			byteBuffer.clear();
+			// 没有内容则关闭通道
+			if (socketChannel.isOpen()) {
+				try {
+					if (socketChannel.read(byteBuffer) == -1) {
+						socketChannel.close();
+					} else {
+						// 将缓冲器转换为读状态
+						byteBuffer.flip();
+						// 将缓冲器中接收到的值按 localCharset 格式编码保存
+						String receivedRequestData = Charset.forName("UTF-8").newDecoder().decode(byteBuffer).toString();
+						System.out.println(socketChannel.getRemoteAddress() + "请求数据: " + receivedRequestData);
+						socketChannel.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
